571fa37b7c02f2efccbe5001788748cb1e8464fe
danielsantiagz <danielsantiagz@gmail.com>
2018-03-22 17:43:25 -0400

    added hexdump function

--- basics/proxy.py
+++ basics/proxy.py
@@ -29,6 +29,19 @@ def server_loop(local_host,local_port,remote_host,remote_port,receive_first):
 
         proxy_thread.start()
 
+# this is a pretty hex dumping function directly taken from
+# the comments here:
+# http://code.activestate.com/recipes/142812-hex-dumper/
+def hexdump(src, length=16):
+    result = []
+    digits 4 if instance(src, unicode) else 2
+    for i in xrange(0,len(src), length):
+        s = src[i:i+length]
+        hexa = b' '.join(["%0*X" % (digits, ord(x)) for x in s])
+        text = b''.join([x if 0x20 <= ord(x) < 0x7F else b'.' for x in s])
+        result.append( b"%04X %-*s %s" % (i, length*(digits + 1), hexa,text) )
+    print b'\n'.join(result)
+
 def proxy_handler(client_socket, remote_host, remote_port, receive_first):
 
     # connect to the remote host
