acff4a4ffeb32f6626753a976fc07abd051cd159
danielsantiagz <danielsantiagz@gmail.com>
2018-03-30 22:35:05 -0400

    forgot to commit

--- rsockets/sniffer_with_icmp.py
+++ rsockets/sniffer_with_icmp.py
@@ -76,3 +76,9 @@ class ICMP(Structure):
         ("unused",      c_ushort),
         ("next_hop_mtu", c_ushort)
     ]
+    def __new__(self, socket_buffer):
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer):
+        pass
+    
